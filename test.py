7044a1f7a7b3dbe4cc174809f1cc735228535ccf
Alvaro-USC <alvaro.schwiedop@rai.usc.es>
2026-04-12 08:14:18 +0200

    Update

--- test.py
+++
@@ -1,2 +0,0 @@
-import os, re; p=re.compile(r'[𐀀-􏿿]'); print('
-'.join(f'{r}/{f}' for r, d, files in os.walk('.') for f in files if f.endswith('.html') and p.search(open(os.path.join(r,f), encoding='utf-8').read())))
